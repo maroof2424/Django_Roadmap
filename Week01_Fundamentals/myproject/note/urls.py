850c112fd61af9e4a2f688bb24c042b13dc7b13f
maroof2424 <maroof96965@gmail.com>
2025-09-29 20:28:47 +0500

    week1 day7

--- Week01_Fundamentals/myproject/note/urls.py
+++ Week01_Fundamentals/myproject/note/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path 
+# from django.urls import path 
+# from . import views
+
+# urlpatterns = [
+#     path("", views.home, name="home"),
+#     path("contact/", views.contact, name="contact"),
+#     path("about/", views.about, name="about"),
+# ]
+
+
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("contact/", views.contact, name="contact"),
-    path("about/", views.about, name="about"),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('notes/', views.notes_list, name='notes_list'),
 ]
--- Week01_Fundamentals/myproject/note/views.py
+++ Week01_Fundamentals/myproject/note/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import HttpResponse,render
-
+from .models import Note
 # def home(request):
 #     return HttpResponse("Hello, Django! this is my first view")
 
@@ -8,5 +8,8 @@ def home(request):
     return render(request,"note/index.html")
 def about(request):
     return render(request,"note/about.html")
-def contact(request):
-    return render(request,"note/contact.html")
+# def contact(request):
+#     return render(request,"note/contact.html")
+def notes_list(request):
+    notes = Note.objects.all()
+    return render(request, "note/notes_list.html", {"notes": notes})
