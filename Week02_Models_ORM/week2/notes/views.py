c29b65597304ec3c9a2c50f6069e268d3187c684
maroof2424 <maroof96965@gmail.com>
2025-10-02 21:27:19 +0500

    Week2 day3

--- Week02_Models_ORM/week2/notes/views.py
+++ Week02_Models_ORM/week2/notes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Note
 # Create your views here.
+
+def notes_list(request):
+    notes = Note.objects.all().order_by("-created_at")
+    return render(request,"notes/notes_list.html", {"notes": notes})
