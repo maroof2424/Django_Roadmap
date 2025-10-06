dbaef54d9941ecb2e21d8d9378eb2e80471872d2
maroof2424 <maroof96965@gmail.com>
2025-10-06 20:46:16 +0500

    Week2 Day7

--- Week02_Models_ORM/week2/notes/demo.py
+++ Week02_Models_ORM/week2/notes/demo.py
@@ -1,21 +1,13 @@
-import os
-import sys
-import django
+
 from faker import Faker
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "week2.settings")
-
-django.setup()
-
 from notes.models import Note
 
 fake = Faker()
-for _ in range(50):
-    Note.objects.create(
-        title=fake.sentence(nb_words=5),
-        content=fake.paragraph(nb_sentences=5),
-    )
+def Create_data(n = 50):
+    for _ in range(n):
+        Note.objects.create(
+            title=fake.sentence(nb_words=5),
+            content=fake.paragraph(nb_sentences=5),
+        )
 
-print("✅ Data created successfully")
+    print("✅ Data created successfully")
