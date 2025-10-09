883ad0b780fb64265369ca6b6c3632dabd9ab722
maroof2424 <maroof96965@gmail.com>
2025-10-09 15:57:22 +0500

    week3 day3

--- Week03_Admin_Forms/Week03_Admin_Forms/feedback/forms.py
+++ Week03_Admin_Forms/Week03_Admin_Forms/feedback/forms.py
@@ -4,10 +4,10 @@ from .models import Feedback
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
-        fields = ["name", "email", "rating", "message"]
+        fields = ['name', 'email', 'rating', 'message']
         widgets = {
-            "name": forms.TextInput(attrs={"class": "form-control", "placeholder": "Enter Your Name"}),
-            "email": forms.EmailInput(attrs={"class": "form-control", "placeholder": "Enter Your Email"}),
-            "rating": forms.NumberInput(attrs={"class": "form-control"}),
-            "message": forms.Textarea(attrs={"class": "form-control", "rows": 5, "placeholder": "Your feedback"}),
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}),
+            'rating': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 5}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Your Feedback'}),
         }
