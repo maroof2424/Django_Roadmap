925d4e3849e40db3dcbe16b9ebaeff2a07701279
maroof2424 <maroof96965@gmail.com>
2025-10-10 20:30:02 +0500

    Week3 Day4

--- Week03_Admin_Forms/Week03_Admin_Forms/feedback/forms.py
+++ Week03_Admin_Forms/Week03_Admin_Forms/feedback/forms.py
@@ -11,3 +11,15 @@ class FeedbackForm(forms.ModelForm):
             'rating': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 5}),
             'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Your Feedback'}),
         }
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not email.endswith(".com"):
+            raise forms.ValidationError("Email must end with .com") 
+        return email
+    
+    def clean_rating(self):
+        rating = self.cleaned_data.get("rating")
+        if rating < 1 or rating > 5:
+            raise forms.ValidationError("rating Must be between 1-5") 
+        return rating
