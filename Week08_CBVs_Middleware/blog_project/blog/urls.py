c161af10bb0e3981a1cda68bd11ce0e70e7be829
maroof2424 <maroof96965@gmail.com>
2025-11-13 16:20:08 +0500

    Week8 Day3

--- Week08_CBVs_Middleware/blog_project/blog/urls.py
+++ Week08_CBVs_Middleware/blog_project/blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.list_post.as_view(),name="list_post"),
-    path("post_details/<int:pk>/",views.post_details.as_view(),name="post_details"),
+    path("",views.ListPostView.as_view(),name="list_post"),
+    path("post_details/<int:pk>/",views.PostDetailsView.as_view(),name="post_details"),
+    path("create_post/new/",views.CreatePostView.as_view(),name="create_post"),
+    path("update_post/<int:pk>/",views.UpdatePostView.as_view(),name="update_post"),
+    path("delete_post/<int:pk>/",views.DeletePostView.as_view(),name="delete_post"),
 ]
--- Week08_CBVs_Middleware/blog_project/blog/views.py
+++ Week08_CBVs_Middleware/blog_project/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView,DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,CreateView, UpdateView, DeleteView
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 # base_dir = "blog/"
@@ -11,13 +13,30 @@ from .models import Post
 #         posts = Post.objects.all().order_by("-created_at","-updated_at")
 #         return render(request,self.template_name,{"posts":posts})
 
-class list_post(ListView):
+class ListPostView(ListView):
     model = Post
     template_name = "blog/list_post.html"
     context_object_name = "posts"
     ordering = ["-created_at","-updated_at"]
 
-class post_details(DetailView):
+class PostDetailsView(DetailView):
     model = Post
     template_name = "blog/post_details.html"
     context_object_name = "post"
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm 
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("list_post")
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = PostForm 
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("list_post")
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("list_post")
